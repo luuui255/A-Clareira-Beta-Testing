7db21f3fd4c1d66c0f17cc29c706a2d49587aee7
luuui255 <lui2504.almeida@gmail.com>
2026-09-09 11:14:04 -0400

    Add files via upload

---
+++ site beta tester/links.js
@@ -0,0 +1,7 @@
+// Links da campanha. Se o Play Console gerar outro endereço para a
+// permissão de outra faixa, altere somente TEST_LINKS.permission.
+const TEST_LINKS = Object.freeze({
+  group: "https://groups.google.com/g/a-clareira-testers",
+  permission: "https://play.google.com/apps/testing/com.iceheadstudio.aclareira",
+  store: "https://play.google.com/store/apps/details?id=com.iceheadstudio.aclareira"
+});
